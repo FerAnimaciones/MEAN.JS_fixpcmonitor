b849b5d8e48138c2f942d9ee36fc6f3bf52bf774
Fernando Manuel Avila Cataño <feranimaciones@gmail.com>
2017-05-11 00:13:09 -0600

    Actualization 11/05/2017  12:13 am

--- modules/articles/server/controllers/articles.server.controller.js
+++ modules/articles/server/controllers/articles.server.controller.js
@@ -81,7 +81,7 @@ exports.delete = function (req, res) {
  * List of Articles
  */
 exports.list = function (req, res) {
-  Article.find().sort('-created').populate('user', 'displayName').exec(function (err, articles) {
+  Article.find().sort('-creado').populate('user', 'displayName').exec(function (err, articles) {
     if (err) {
       return res.status(422).send({
         message: errorHandler.getErrorMessage(err)
